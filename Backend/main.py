13f4af03210b1781115618be237114ee4fab2fbb
saiiiif <saifarmyej@gmail.com>
2024-03-22 23:07:24 +0100

    first commit

---
+++ Backend/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from joblib import load
+from pydantic import BaseModel
+import numpy as np
+
+app = FastAPI()
+model = load('model.joblib')
+
+class InputData(BaseModel):
+    feature: list
+
+
+@app.post("/predict/")
+def predict(data: InputData):
+    input_data = np.array(data.feature).reshape(1,-1)
+    prediction = model.predict(input_data)
+    return {"prediction": prediction.tolist()}
+
+
+@app.get("/hallo")
+async def root():
+    return {"hello":"world"}
